2288fd56f1e22c2d6ba828c0e8e6ae9b0f3bed03
Ilya758 <subscriber6436@gmail.com>
2022-11-19 10:12:37 +0300

    feat: implemented basic server structure

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,63 @@
-export default (): void => {};
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import express from 'express';
+import morgan from 'morgan';
+import { connect } from 'mongoose';
+import { IController } from './models/IController';
+import errorMiddleware from './middlewares/error.middleware';
+import { DEFAULT_PORT } from './common/enums/defaultPort';
+import * as config from './config';
+
+const {
+  default: { connectionString, nodeEnv, port },
+} = config;
+
+export default class App {
+  private app: express.Application;
+
+  constructor(controllers: IController[]) {
+    this.app = express();
+
+    this.connectToTheDatabase();
+    this.initializeMiddlewares();
+    this.initializeControllers(controllers);
+    this.initializeErrorHandling();
+  }
+
+  private initializeMiddlewares = (): void => {
+    if (nodeEnv !== 'prod') {
+      this.app.use(morgan('dev'));
+    }
+
+    this.app.use(bodyParser.json());
+    this.app.use(cookieParser());
+    this.app.use(compression());
+    this.app.use(cors());
+  };
+
+  private initializeControllers = (controllers: IController[]): void => {
+    controllers.forEach(({ router }) => {
+      this.app.use('/', router);
+    });
+  };
+
+  private initializeErrorHandling = (): void => {
+    this.app.use(errorMiddleware);
+  };
+
+  private connectToTheDatabase = (): void => {
+    if (connectionString)
+      connect(connectionString).catch((err: Error) => {
+        console.error(err.stack);
+        process.exit(1);
+      });
+  };
+
+  public listen = (): void => {
+    this.app.listen(port, () => {
+      console.log(`Application was running on port ${port || DEFAULT_PORT}`);
+    });
+  };
+}
