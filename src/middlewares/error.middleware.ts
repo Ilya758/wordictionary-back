60e6bec498c65c711aa1332402e4247e24cf96db
Ilya758 <subscriber6436@gmail.com>
2022-11-19 10:28:32 +0300

    fix: provided next cb to error middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,12 +1,17 @@
-
-import { Request, Response } from 'express';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from 'express';
 import HttpCodes from '../common/enums/httpCodes';
 import HttpException from '../exceptions/httpException';
 
-export default (error: HttpException, _: Request, response: Response): void => {
+export default (
+  error: HttpException,
+  _: Request,
+  response: Response,
+  __: NextFunction
+): void => {
   const status = error.status || HttpCodes.InternalServerError;
   const message = error.message || 'Something went wrong';
-  
+
   response.status(status).send({
     message,
     status,
