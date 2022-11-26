10fe01025d9e1e3ca0210dd550a760790ff7fec4
Ilya758 <subscriber6436@gmail.com>
2022-11-26 11:04:47 +0300

    fix: removed useless eslint rule, fixed interface IUser

--- src/models/users/user.interface.ts
+++ src/models/users/user.interface.ts
@@ -1,5 +1,6 @@
-export interface IUser {
-  _id: string;
+import { IId } from '../../common/enums/models/interfaces/IId';
+
+export interface IUser extends IId {
   firstName: string;
   lastName: string;
   email: string;
